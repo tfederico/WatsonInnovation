892fab5aa090804f0e8dc61cd5d98b21827dd969
federico <federicotavella.7@gmail.com>
2017-12-21 17:57:31 +0100

    Add UI mockup

---
+++ Pearl/app/src/main/java/com/example/rangs/pearl/PictureFragment.java
@@ -0,0 +1,28 @@
+package com.example.rangs.pearl;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ */
+public class PictureFragment extends android.support.v4.app.Fragment {
+
+
+    public PictureFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_picture, container, false);
+    }
+
+}
---
+++ Pearl/app/src/main/java/com/example/rangs/pearl/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.rangs.pearl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by rangs on 12/20/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+    private static int splashInterval = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+            }
+        }, splashInterval);
+    }
+}
